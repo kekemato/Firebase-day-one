680974212612c41880850ac7b00701678d8fea55
Magdalena Brzozowska <magdalena.brzozowska94@wp.pl>
2018-11-28 11:17:33 +0100

    firebase added and configured

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase'
+
+const config = {
+    apiKey: "AIzaSyBpl-bhHeCVRfBUZ948LS-qpjGkOPDfCI8",
+    authDomain: "poniedzialek-26.firebaseapp.com",
+    databaseURL: "https://poniedzialek-26.firebaseio.com",
+    projectId: "poniedzialek-26",
+    storageBucket: "poniedzialek-26.appspot.com",
+    messagingSenderId: "77751688488"
+  }
+
+  firebase.initializeApp(config)
+
+  export const database = firebase.database()
